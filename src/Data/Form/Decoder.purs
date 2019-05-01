f1e84a82bccbeebd7d42d9a223ba4684d5768ac1
amderbar <amderbar@gmail.com>
2019-05-01 18:22:06 +0900

    make core module and test

---
+++ src/Data/Form/Decoder.purs
@@ -0,0 +1,139 @@
+module Data.Form.Decoder where
+
+import Prelude                   hiding (when)
+import Control.Alt                      (class Alt, (<|>))
+import Control.Alternative              (class Alternative)
+import Control.Plus                     (class Plus, empty)
+import Data.Either               as      Either
+import Data.Int                  as      Int
+import Data.Number               as      Num
+import Data.String               as      Str
+import Data.Validation.Semigroup        (V(..))
+import Data.Validation.Semigroup as      V
+
+-- | Core type representing a decoder.
+-- |  It decodes input into type `input`, raising error of type `err`.
+newtype Decoder err input output
+  = Decoder (input -> V err output)
+
+instance functorDecoder :: Functor (Decoder e i) where
+  map f (Decoder v) = Decoder $ (map f) <<< v
+
+instance applyDecoder :: Semigroup e => Apply (Decoder e i) where
+  apply pre cont = Decoder $ \i -> (run pre i) <*> (run cont i)
+
+instance applicativeDecoder :: Semigroup e => Applicative (Decoder e i) where
+  pure a = Decoder $ const (pure a)
+
+instance bindDecoder :: Semigroup e => Bind (Decoder e i) where
+  bind pre cont = Decoder \i -> (run pre i) `V.andThen` \o -> run (cont o) i
+
+instance monadDecoder :: Semigroup e => Monad (Decoder e i)
+
+instance semigroupiodDecoder :: Semigroupoid (Decoder e) where
+  compose g f = Decoder \i -> (run f i) `V.andThen` \o -> run g o
+
+instance semigroupDecoder :: (Semigroup e, Semigroup o) => Semigroup (Decoder e i o) where
+  append f g = Decoder \i -> (run f i) <> (run g i)
+
+instance monoidDecoder :: (Monoid e, Monoid o) => Monoid (Decoder e i o) where
+  mempty =  Decoder \_ -> mempty
+
+instance altDecoder :: Alt (Decoder e i) where
+  alt f g = Decoder \i -> V $ (V.toEither $ run f i) <|> (V.toEither $ run g i)
+
+instance plusDecoder :: Plus m => Plus (Decoder (m e) i) where
+  empty = fail empty
+
+instance alternativeDecoder :: (Plus m, Semigroup (m e)) => Alternative (Decoder (m e) i)
+
+-- | An alias for special decoder that does not produce any outputs.
+-- | It is used for just validating inputs.
+type Validator err input = Decoder err input Unit
+
+
+-- Decode functions
+
+-- | Basic function that decodes input by given decoder.
+run :: forall e i o. Decoder e i o -> i -> V e o
+run (Decoder f) a = f a
+
+-- | Primitive decoder that always succeeds with input as it is.
+identity :: forall e i. Semigroup e => Decoder e i i
+identity = Decoder pure
+
+-- | Primitive decoder that always succeeds with constant value.
+always :: forall e i o. Semigroup e => o -> Decoder e i o
+always a = Decoder $ const (pure a)
+
+-- | Primitive decoder which always results to invalid.
+fail :: forall e i o. e -> Decoder e i o
+fail err = Decoder $ const (V.invalid err)
+
+-- | Decoder into `Int`, raising `err` when a input is invalid for an integer.
+int :: forall e. e -> Decoder e String Int
+int err = Decoder $ V <<< Either.note err <<< Int.fromString
+
+-- | Decoder into `Number`, raising `err` when a input is invalid for a number.
+number :: forall e. e -> Decoder e String Number
+number err = Decoder $ V <<< Either.note err <<< Num.fromString
+
+-- | Decoder into given data type, raising `err` when a input is invalid for the data.
+symbol :: forall e s. Semigroup e => e -> s -> String -> Decoder e String s
+symbol err sybl str = Decoder \i -> if i == str then pure sybl else V.invalid err
+
+
+-- Primitive validators
+
+-- | internal validation function using given function.
+validater :: forall e i. Semigroup e => e -> (i -> Boolean) -> i -> V e Unit
+validater err rule val =
+  if rule val
+    then pure unit
+    else V.invalid err
+
+-- | Primitive validator limiting by not null.
+required :: forall e. Semigroup e => e -> Validator e String
+required err = Decoder $ validater err (not <<< Str.null)
+
+-- | Primitive validator limiting by minimum bound.
+minBound :: forall e ord. Semigroup e => Ord ord => e -> ord -> Validator e ord
+minBound err bound = Decoder $ validater err (_ >= bound)
+
+-- | Primitive validator limiting by maximum bound.
+maxBound :: forall e ord. Semigroup e => Ord ord => e -> ord -> Validator e ord
+maxBound err bound = Decoder $ validater err (_ <= bound)
+
+-- | Primitive validator limiting by minimum length.
+minLength :: forall e. Semigroup e => e -> Int -> Validator e String
+minLength err bound = Decoder $ validater err (\s -> Str.length s >= bound)
+
+-- | Primitive validator limiting by maximum length.
+maxLength :: forall e. Semigroup e => e -> Int -> Validator e String
+maxLength err bound = Decoder $ validater err (\s -> Str.length s <= bound)
+
+
+-- Helper functions to validation
+
+-- | Apply validator on given decoder.
+-- | If a input is invalid for given validator, decoding fails.
+assert :: forall e i o. Semigroup e => Validator e i -> Decoder e i o -> Decoder e i o
+assert v d = v >>= (\_ -> d)
+
+-- | Only checks validity if a condition is `true`.
+when :: forall e i. Semigroup e => (i -> Boolean) -> Validator e i -> Validator e i
+when c v =
+  Decoder \i -> if c i
+    then run v i
+    else pure unit
+
+-- | Only checks validity unless a condition is `true`.
+unless :: forall e i. Semigroup e => (i -> Boolean) -> Validator e i -> Validator e i
+unless g = when (not <<< g)
+
+
+-- Function to build up decoder for forms
+
+-- | `hoist` is mainly used for accessing sub model of target value.
+hoist :: forall i j e o. (j -> i) -> Decoder e i o -> Decoder e j o
+hoist f (Decoder g) = Decoder (g <<< f)
---
+++ test/Main.purs
@@ -0,0 +1,125 @@
+module Test.Main where
+
+import Prelude                      (class Eq, class Semigroup, class Show, Unit, discard, negate, unit, ($), (<), (<<<))
+import Data.Either                  (Either(..))
+import Data.Form.Decoder          as D
+import Data.Generic.Rep             (class Generic)
+import Data.Generic.Rep.Show        (genericShow)
+import Data.Maybe                   (Maybe(..))
+import Data.Validation.Semigroup  as V
+import Effect                       (Effect)
+import Test.Spec                    (describe, it)
+import Test.Spec.Assertions         (shouldEqual)
+import Test.Spec.Reporter.Console   (consoleReporter)
+import Test.Spec.Runner             (run)
+
+data TestType
+  = TypeA
+  | TypeB
+
+derive instance eqTestType :: Eq TestType
+derive instance genericTestType :: Generic TestType _
+instance showTestType :: Show TestType where
+  show = genericShow
+
+main :: Effect Unit
+main =
+  run [consoleReporter] do
+    describe "Data.Form.Decoder" do
+      describe "Decode functions" do
+        it "run" do
+          (V.toEither $ D.run (D.int "Invalid") "foo") `shouldEqual` (Left "Invalid")
+          (V.toEither $ D.run (D.int "Invalid") "34")  `shouldEqual` (Right 34)
+
+        it "identity" do
+          (runIdentity "foo") `shouldEqual` (Right "foo")
+          (runIdentity 34)    `shouldEqual` (Right 34)
+
+        it "always" do
+          (runAlways "bar" "foo") `shouldEqual` (Right "bar")
+          (runAlways 34 23)       `shouldEqual` (Right 34)
+
+        it "fail" do
+          (runFail ["error"] "foo")     `shouldEqual` (Left ["error"])
+          (runFail ["error"] (Just 34)) `shouldEqual` (Left ["error"])
+
+        it "int" do
+          (V.toEither $ D.run (D.int "Invalid") "foo")  `shouldEqual` (Left "Invalid")
+          (V.toEither $ D.run (D.int "Invalid") "34.3") `shouldEqual` (Left "Invalid")
+          (V.toEither $ D.run (D.int "Invalid") "34e3") `shouldEqual` (Left "Invalid")
+          (V.toEither $ D.run (D.int "Invalid") "34")   `shouldEqual` (Right 34)
+
+        it "number" do
+          (V.toEither $ D.run (D.number "Invalid") "foo")  `shouldEqual` (Left  "Invalid")
+          (V.toEither $ D.run (D.number "Invalid") "34.3") `shouldEqual` (Right (34.3    :: Number))
+          (V.toEither $ D.run (D.number "Invalid") "34e3") `shouldEqual` (Right (34000.0 :: Number))
+          (V.toEither $ D.run (D.number "Invalid") "34")   `shouldEqual` (Right (34.0    :: Number))
+
+        it "symbol" do
+          (V.toEither $ D.run (D.symbol "Invalid" TypeA "TypeA") "foo"  ) `shouldEqual` (Left  "Invalid")
+          (V.toEither $ D.run (D.symbol "Invalid" TypeB "TypeB") "34.3" ) `shouldEqual` (Left  "Invalid")
+          (V.toEither $ D.run (D.symbol "Invalid" TypeA "TypeA") "TypeA") `shouldEqual` (Right TypeA)
+          (V.toEither $ D.run (D.symbol "Invalid" TypeB "TypeB") "TypeB") `shouldEqual` (Right TypeB)
+
+      describe "Primitive validators" do
+        it "validater" do
+          (V.toEither $ D.validater "Invalid" (\_ -> false) "foo") `shouldEqual` (Left  "Invalid")
+          (V.toEither $ D.validater "Invalid" (\_ -> true)  "foo") `shouldEqual` (Right unit)
+
+        it "required" do
+          (V.toEither $ D.run (D.required "Invalid") "")    `shouldEqual` (Left  "Invalid")
+          (V.toEither $ D.run (D.required "Invalid") "foo") `shouldEqual` (Right unit)
+
+        it "minBound" do
+          (V.toEither $ D.run (D.minBound "Invalid" 0) (-1)) `shouldEqual` (Left  "Invalid")
+          (V.toEither $ D.run (D.minBound "Invalid" 0) 0)    `shouldEqual` (Right unit)
+
+        it "maxBound" do
+          (V.toEither $ D.run (D.maxBound "Invalid" 0) 1) `shouldEqual` (Left  "Invalid")
+          (V.toEither $ D.run (D.maxBound "Invalid" 0) 0) `shouldEqual` (Right unit)
+
+        it "minLength" do
+          (V.toEither $ D.run (D.minLength "Invalid" 6) "short")  `shouldEqual` (Left  "Invalid")
+          (V.toEither $ D.run (D.minLength "Invalid" 6) "looong") `shouldEqual` (Right unit)
+
+        it "maxLength" do
+          (V.toEither $ D.run (D.maxLength "Invalid" 5) "looong") `shouldEqual` (Left  "Invalid")
+          (V.toEither $ D.run (D.maxLength "Invalid" 5) "short")  `shouldEqual` (Right unit)
+
+      describe "Helper functions to validation" do
+        it "assert" do
+          (V.toEither $ D.run testAssert "")    `shouldEqual` (Left  "Required")
+          (V.toEither $ D.run testAssert "foo") `shouldEqual` (Left  "Invalid")
+          (V.toEither $ D.run testAssert "34")  `shouldEqual` (Right 34)
+
+        it "when" do
+          (runFailWhen (_ < 0) ["error"] (-1)) `shouldEqual` (Left ["error"])
+          (runFailWhen (_ < 0) ["error"] 0)    `shouldEqual` (Right unit)
+
+        it "unless" do
+          (runFailUnless (_ < 0) ["error"] 0)    `shouldEqual` (Left ["error"])
+          (runFailUnless (_ < 0) ["error"] (-1)) `shouldEqual` (Right unit)
+
+      describe "Function to build up decoder for forms" do
+        it "hoist" do
+          (V.toEither $ D.run (D.hoist _.f1 $ D.int "Invalid") { f1: "foo", f2: "34" }) `shouldEqual` (Left  "Invalid")
+          (V.toEither $ D.run (D.hoist _.f2 $ D.int "Invalid") { f1: "foo", f2: "34" }) `shouldEqual` (Right 34)
+
+    where
+      runIdentity :: forall a. a -> Either String a
+      runIdentity = V.toEither <<< (D.run D.identity)
+      
+      runAlways :: forall a b. a -> b -> Either String a
+      runAlways o = V.toEither <<< D.run (D.always o)
+
+      runFail :: forall a b. a -> b -> Either a b
+      runFail o = V.toEither <<< D.run (D.fail o)
+
+      runFailWhen :: forall e i. Semigroup e => (i -> Boolean) -> e -> i -> Either e Unit
+      runFailWhen f o = V.toEither <<< D.run (D.when f $ D.fail o)
+
+      runFailUnless :: forall e i. Semigroup e => (i -> Boolean) -> e -> i -> Either e Unit
+      runFailUnless f o = V.toEither <<< D.run (D.unless f $ D.fail o)
+
+      testAssert :: D.Decoder String String Int
+      testAssert = D.assert (D.required "Required") (D.int "Invalid")
